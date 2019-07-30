dedd3dc69ba3768d767ec0c855a493a21657b13a
spandan <spandan.99roomz@gmail.com>
2019-07-30 19:45:15 +0530

    introduced test cases to test diagonals and antidiagonals

--- tests/test_all_cases.py
+++ tests/test_all_cases.py
@@ -89,6 +89,51 @@ class TestAllCases(unittest.TestCase):
 		self.assertFalse(r[0])
 		self.assertEqual(r[1], 'Occupied')
 
+	def testDiagonal(self):
+		Game = TicTacToe()
+
+		r = Game.move("X", 0)
+		self.assertTrue(r[0])
+		self.assertEqual(r[1], 'Continue')
+
+		r = Game.move("O", 3)
+		self.assertTrue(r[0])
+		self.assertEqual(r[1], 'Continue')
+
+		r = Game.move("X", 8)
+		self.assertTrue(r[0])
+		self.assertEqual(r[1], 'Continue')
+
+		r = Game.move("O", 6)
+		self.assertTrue(r[0])
+		self.assertEqual(r[1], 'Continue')
+
+		r = Game.move("X", 4)
+		self.assertTrue(r[0])
+		self.assertEqual(r[1], 'Won')
+
+	def testAntiDiagonal(self):
+		Game = TicTacToe()
+
+		r = Game.move("X", 2)
+		self.assertTrue(r[0])
+		self.assertEqual(r[1], 'Continue')
+
+		r = Game.move("O", 1)
+		self.assertTrue(r[0])
+		self.assertEqual(r[1], 'Continue')
+
+		r = Game.move("X", 6)
+		self.assertTrue(r[0])
+		self.assertEqual(r[1], 'Continue')
+
+		r = Game.move("O", 5)
+		self.assertTrue(r[0])
+		self.assertEqual(r[1], 'Continue')
+
+		r = Game.move("X", 4)
+		self.assertTrue(r[0])
+		self.assertEqual(r[1], 'Won')
 
 if __name__ == '__main__': 
 	unittest.main()
