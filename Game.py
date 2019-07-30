7a873fb9ac31c9e0d11efe455dfa5511df850b44
Spandan <developer.spandan@gmail.com>
2019-07-30 17:36:52 +0530

    Removed print statements, code made simpler and readable

--- Game.py
+++ Game.py
@@ -2,13 +2,6 @@
 class TicTacToe():
 
 	board = []
-	sign1 = "",
-	sign2 = ""
-	boardDesign = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8]
-	]
 	winning_patterns = [
 		[0, 1, 2],
 		[3, 4, 5],
@@ -19,108 +12,49 @@ class TicTacToe():
 		[0, 4, 8],
 		[2, 4, 6]
 	]
-	currentMove = -1
-	currentPos = None
-	currentSign = None
-	moveCount = -1
-	winningPattern = None
 
-	def __init__(self, sign1, sign2):
+	moveCount = 0
+
+	def __init__(self):
 		self.board = ["N"]*9
-		self.sign1 = sign1
-		self.sign2 = sign2
 
-
-	def play1(self, pos):
-
-		if self.currentMove == -1 or self.currentMove == 2:
-			self.currentMove = 1
-			self.play(self.sign1, pos)
-		else:
-			print("Invalid move by Player One, Now is player Two chance to move")
-			exit()
-
-	def play2(self, pos):
-		if self.currentMove == -1 or self.currentMove == 1:
-			self.currentMove = 2
-			self.play(self.sign2, pos)
-		else:
-			print("Invalid Move by Player Two, Now is player One chance to move")
-			exit()
-
-	def play(self, sign, pos):
-		
-		self.currentPos = pos
-		self.currentSign = sign
-
-		if(self.board[pos] != "N"):
-			print("Invalid move by Player "+ self.getPlayer() +" at position:", pos)
-			exit()
-
-		self.board[pos] = sign
-		print("Player "+ self.getPlayer() + " puts " + sign + " at position " + str(pos))
-		self.move()
-
-	def move(self):
+	def move(self, sign, pos):
 		
 		self.moveCount += 1 
 
-		if self.isResult():
-			self.showResult()
-			self.printBoard()
+		#Check if the position has occupied
+		if(self.isOccupied(pos)):
+			return (False, 'Occupied')
 
-		elif self.isFull():
-			print("\nGame Result : Its a Tie")
-			self.printBoard()
+		self.board[pos] = sign
 
-	def isResult(self):
+		#Check if the board is full
+		if self.isFull():
+			return (True, 'Tie')
+
+		#Check if anybody has won
+		if(self.isWon(sign, pos)):
+			return (True, 'Won')
+
+		return (True, 'Continue')
+
+	def isWon(self, sign, pos):
 		
-		i = -1
-		for patterns in self.winning_patterns:
-			i+=1
+		for pattern in self.winning_patterns:
 
-			if self.currentPos not in patterns:
+			if pos not in pattern:
 				continue
 
-			s = self.board[patterns[0]]
-			
-			if s != self.currentSign:
-				continue
-
-			flag = True
-			for p in patterns[1:]:
-				if self.board[p] == "N" or self.board[p] != s:
-					flag = False
-					break
-			if(flag):
-				self.winningPattern = patterns
+			t = filter(lambda x: self.board[x] != sign, pattern)
+		
+			if not list(t):
 				return True
 
 		return False
-			
 
-	def showResult(self):
-		print(
-			"\nGame Result : \nPlayer "+ self.getPlayer() + 
-			" wins by pattern -> " + str(self.winningPattern) + 
-			" on move number " + str(self.moveCount)
-		)
-
-	def getPlayer(self):
-		return self.currentSign == self.sign1 and "One" or "Two"  
-		
+	def isOccupied(self, pos):
+		return self.board[pos] != "N"
 
 	def isFull(self):
-		return self.moveCount == 8
-
-	def printBoard(self):
-		print("\nBoard :\n")
-		for pattern in self.boardDesign:
-			for p in pattern:
-				print(self.board[p]+" ", end = '')
-			print("\n")
-		self.exit()
-
-	def exit(self):
-		exit()
+		return self.moveCount == 9
 
--- app.py
+++ app.py
@@ -2,21 +2,20 @@ from Game import TicTacToe
 
 if __name__ == '__main__':
 
-	player1Sign = "X"
-	player2Sign = "O"
+	Game = TicTacToe()
 
-	Game = TicTacToe(player1Sign, player2Sign)
-	
-	Game.play1(0)
-	Game.play2(4)
-	
-	Game.play1(6)
-	Game.play2(3)
-	
-	Game.play1(5)
-	Game.play2(7)
+	#Whichever sign moves first will automatically becomes player 1 sign
 
-	Game.play1(8)
-	Game.play2(1)
-	
-	Game.play1(2)
+	print(Game.move("X",0))
+	print(Game.move("O",4))
+
+	print(Game.move("X",6))
+	print(Game.move("O",3))
+
+	print(Game.move("X",5))
+	print(Game.move("O",7))
+
+	print(Game.move("X",8))
+	print(Game.move("O",1))
+
+	print(Game.move("X",2))
